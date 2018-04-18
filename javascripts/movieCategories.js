232fd42c1640b559e205659b2bb3ce8efabc3ad9
Maken Hubbard <makhub26@outlook.com>
2018-04-18 18:28:31 -0500

    have the xhr requests typed up and exporting

--- javascripts/movieCategories.js
+++ javascripts/movieCategories.js
@@ -1,1 +1,10 @@
 // xhr categories.json
+const loadCategories = (loadCategoriesFunction, errorCategoriesFunction) => {
+  const myRequest = new XMLHttpRequest();
+  myRequest.addEventListener('load', loadCategoriesFunction);
+  myRequest.addEventListener('error', errorCategoriesFunction);
+  myRequest.open('GET', '../db/categories.json');
+  myRequest.send();
+};
+
+module.exports = loadCategories();
--- javascripts/movieElements.js
+++ javascripts/movieElements.js
@@ -1,1 +1,10 @@
 // xhr movieElements.json
+const loadElements = (loadElementsData, errorElementFunction) => {
+  const myRequest = new XMLHttpRequest();
+  myRequest.addEventListener('load', loadElementsData);
+  myRequest.addEventListener('error', errorElementFunction);
+  myRequest.open('GET', '../db/movieElements.json');
+  myRequest.send();
+};
+
+module.exports = loadElements();
