0139cc861a1c6c027155108e8efce97cc084e121
Maken Hubbard <makhub26@outlook.com>
2018-04-18 21:06:51 -0500

    have main.js and and some stuff in the data.js

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,1 +1,26 @@
 // manipulate data , call xhrs
+let categoriesArray = [];
+let elementsArray = [];
+
+const setCategories = (goryArray) => {
+  categoriesArray = goryArray;
+};
+
+const getCategories = () => {
+  return categoriesArray;
+};
+
+const setElements = (mentsArray) => {
+  elementsArray = mentsArray;
+};
+
+const getElements = () => {
+  return elementsArray;
+};
+
+module.exports = {
+  setCategories,
+  getCategories,
+  setElements,
+  getElements,
+};
